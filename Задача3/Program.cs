a46ec4976ec72e02608ea90e0bd33c32b5e93ed1
Viktoria <vinokurova.94@mail.ru>
2023-10-14 20:04:15 +0300

    add Задача3

---
+++ Задача3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+Console.WriteLine("введите число");
+int input = Convert.ToInt32(Console.ReadLine());
+int minusInput = -input;
+while (input >= minusInput)
+{
+Console.Write(minusInput);
+
+minusInput += 1;
+}
