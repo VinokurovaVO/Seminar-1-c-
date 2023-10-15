55ac2ea0a140e93cf61bc9f40655cff9b29cd93f
Viktoria <vinokurova.94@mail.ru>
2023-10-15 11:34:07 +0300

    add Домашняя работа к семинару 1 (Задача 5)

---
+++ Задача5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+string? number1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? number2 = Console.ReadLine();
+
+if(Convert.ToInt32(number1) > Convert.ToInt32(number2))
+{
+Console.WriteLine("Первое число больше чем второе число");
+}
+
+if(Convert.ToInt32(number1) < Convert.ToInt32(number2))
+{
+Console.WriteLine("Первое число меньше чем второе число");
+}
+
+if(Convert.ToInt32(number1) == Convert.ToInt32(number2))
+{
+Console.WriteLine("Введеные числа равны");
+}
