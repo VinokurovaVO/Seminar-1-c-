cb821c16ed602526f52a61e71cae62bf0f069ba3
Viktoria <vinokurova.94@mail.ru>
2023-10-15 19:55:57 +0300

    add Домашняя работа к семинару 1 (Задача 7)

--- Задача5/Program.cs
+++ Задача5/Program.cs
@@ -1,19 +1,46 @@
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+
 ﻿Console.WriteLine("Введите первое число");
-string? number1 = Console.ReadLine();
+int firstNumber = Console.ReadLine();
 Console.WriteLine("Введите второе число");
-string? number2 = Console.ReadLine();
+int secondNumber = Console.ReadLine();
 
-if(Convert.ToInt32(number1) > Convert.ToInt32(number2))
+        if(Convert.ToInt32(firstNumber)) > (Convert.ToInt32(secondNumber))
 {
-Console.WriteLine("Первое число больше чем второе число");
+Console.WriteLine("Первое число `firstNumbe` больше чем второе число `secondNumber");
 }
 
-if(Convert.ToInt32(number1) < Convert.ToInt32(number2))
+if(Convert.ToInt32(firstNumber)) < (Convert.ToInt32(secondNumber))
 {
-Console.WriteLine("Первое число меньше чем второе число");
+Console.WriteLine("Первое число `firstNumbe` меньше чем второе число `secondNumber");
 }
 
-if(Convert.ToInt32(number1) == Convert.ToInt32(number2))
+if(Convert.ToInt32(firstNumber)) = (Convert.ToInt32(secondNumber))
 {
-Console.WriteLine("Введеные числа равны");
+Console.WriteLine("Введенные числа равны `firstNumbe`");
+}
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 5;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
 }
---
+++ Задача7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+    if(number % 2 == 0)
+    {
+        Console.WriteLine("Четное");
+    }
+    else 
+    {
+        Console.WriteLine("Нечетное");
+    }
